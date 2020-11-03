7c50cdebd783b03c0e38124c06eb154b15a6a44d
chinanwu <cawudev@gmail.com>
2020-11-03 15:23:50 -0800

    salmon.......... its jpg not png........ salmon..............

--- src/components/FingerspellImg.svelte
+++ src/components/FingerspellImg.svelte
@@ -1,13 +1,12 @@
 <script>
   // Images from https://web.stanford.edu/~dljaffe/Ralph/jrrd.html
     const letterNum = Math.floor(Math.random() * 26);
-    const test = "assets/a-p.jpg";
 </script>
 
 <style>
     .FingerspellImg {
         /*background-image: url(../resources/alphabet/a-p.jpg);*/
-        background-image: url(assets/a-p.png);
+        background-image: url(assets/a-p.jpg);
     }
 </style>
 
